f8752326de66bfef772b0cbb98561cfacb4163ad
Marcel Marnitz <marcelmarnitz@googlemail.com>
2020-02-01 22:25:18 +0100

    Remove HGG stuff from TlsStream

--- src/AdAuth/Stream/TlsStream.php
+++ src/AdAuth/Stream/TlsStream.php
@@ -6,7 +6,26 @@ use AdAuth\SocketConnectException;
 
 class TlsStream implements StreamInterface {
 
-    private static $CaFile = '../ca.crt';
+    private $caFile;
+    private $peerName;
+    private $peerFingerprint;
+
+    public function __construct(string $caFile = null, string $peerName = null, string $peerFingerprint = null) {
+        $this->peerName = $peerName;
+        $this->peerFingerprint = $peerFingerprint;
+
+        if($caFile !== null) {
+            if (!file_exists($caFile)) {
+                throw new \InvalidArgumentException(sprintf('CA certificate file "%s" does not exist', $caFile));
+            }
+
+            if (!is_readable($caFile)) {
+                throw new \InvalidArgumentException(sprintf('CA certificate file "%s" is not readable', $caFile));
+            }
+        }
+
+        $this->caFile = $caFile;
+    }
 
     /**
      * @inheritDoc
@@ -16,11 +35,11 @@ class TlsStream implements StreamInterface {
             'ssl' => [
                 'crypto_method' => STREAM_CRYPTO_METHOD_TLSv1_2_CLIENT,
                 'disable_compression' => true,
-                'peer_name' => 'dc01.hgg02.lokal',
-                'verify_peer' => true,
+                'peer_name' => $this->peerName,
+                'verify_peer' => $this->peerName !== null,
                 'allow_self_signed' => true,
-                'cafile' => realpath(__DIR__) . '/' . static::$CaFile,
-                'peer_fingerprint' => '2fbfb664b75c8c081412b70e333b7cc16324e0be',
+                'cafile' => $this->caFile,
+                'peer_fingerprint' => $this->peerFingerprint,
                 'ciphers' => 'ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA:ECDHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES128-SHA256:DHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES256-GCM-SHA384:AES128-GCM-SHA256:AES256-SHA256:AES128-SHA256:AES256-SHA:AES128-SHA:DES-CBC3-SHA:HIGH:!aNULL:!eNULL:!EXPORT:!DES:!MD5:!PSK:!RC4',
             ]
         ];
@@ -29,7 +48,7 @@ class TlsStream implements StreamInterface {
         $stream = @stream_socket_client('tls://' . $host . ':' . $port, $errno, $errstr, 1, STREAM_CLIENT_CONNECT, $context);
 
         if(!is_resource($stream)) {
-            throw new SocketConnectException('Fehler beim Verbinden: ' . $errstr . ' (code: ' . $errno . ')');
+            throw new SocketConnectException('Error connecting using TLS: ' . $errstr . ' (code: ' . $errno . ')');
         }
 
         return $stream;
