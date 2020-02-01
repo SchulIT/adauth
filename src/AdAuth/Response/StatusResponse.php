6b6f8022a7d16fb15cc2c9a85102913ec0e651da
Marcel Marnitz <marcelmarnitz@googlemail.com>
2020-02-01 22:26:16 +0100

    Use fqcn

--- src/AdAuth/Response/StatusResponse.php
+++ src/AdAuth/Response/StatusResponse.php
@@ -7,7 +7,7 @@ use JMS\Serializer\Annotation as Serializer;
 class StatusResponse extends AbstractResponse {
     /**
      * @Serializer\SerializedName("status")
-     * @Serializer\Type("array<UserStatus>")
+     * @Serializer\Type("array<AdAuth\Response\UserStatus>")
      */
     private $status;
 
